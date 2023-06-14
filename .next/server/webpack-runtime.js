2d0bb9aee3c99911df0e14186a2568de3e0c7a84
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-06-13 22:46:49 -0300

    Cria layout

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9c18db3991e5ed3a")
+/******/ 		__webpack_require__.h = () => ("72d938055d772dc8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -194,7 +194,7 @@
 /******/ 		__webpack_require__.f.require = (chunkId, promises) => {
 /******/ 			// "1" is the signal for "already loaded"
 /******/ 			if(!installedChunks[chunkId]) {
-/******/ 				if("app/page" == chunkId) {
+/******/ 				if("webpack-runtime" != chunkId) {
 /******/ 					installChunk(require("./" + __webpack_require__.u(chunkId)));
 /******/ 				} else installedChunks[chunkId] = 1;
 /******/ 			}
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc12eafffb9a4e49"; }
+/******/ 		__webpack_require__.h = function() { return "2d4b7d0a9b388b2a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
